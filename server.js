83307d88df5ea6e8ff517924c0c906e2133d43ce
Glitch (authentication-pug) <none>
2021-01-11 20:02:39 +0000

    ⛴💸 Updated with Glitch

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const pug = require("pug");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const passport = require("passport");
-
-const ObjectID = require("mongodb").ObjectID;
 const mongo = require("mongodb").MongoClient;
-const LocalStrategy = require("passport-local");
 const cors = require("cors");
+
+const passport = require("passport");
+const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
 //const bcrypt = require('bcrypt');
 
 const app = express();
-
+app.set("view engine", "pug");
 require("dotenv").config();
 
 fccTesting(app); //For FCC testing purposes
@@ -22,20 +22,16 @@ app.use(cors());
 app.use("/public", express.static("/public"));
 //app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set("view engine", "pug");
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-    cookie: {secure: false}
+    cookie: { secure: false }
   })
-   //      {cookie:{secure: false}})
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,7 +39,7 @@ app.use(passport.session());
 // added delays to certain routes as required to pass testing in these cases
 if (process.env.ENABLE_DELAYS)
   app.use((req, res, next) => {
-    switch (req.method) {  
+    switch (req.method) {
       case "GET":
         switch (req.url) {
           case "/logout":
@@ -67,7 +63,7 @@ if (process.env.ENABLE_DELAYS)
     }
   });
 
-
+// connect to mongoDB so we have access for all of our routes
 mongo.connect(
   process.env.DATABASE,
   { useUnifiedTopology: true },
@@ -75,25 +71,29 @@ mongo.connect(
     var db = client.db("pug");
     if (err) {
       console.log(db + "Database error: " + err);
+      app.route("/").get((req, res) => {
+        res.render("pug", { title: err, message: "Login Failed, try again" });
+      });
     } else {
       console.log("Successful database connection");
-
       passport.serializeUser((user, done) => {
         done(null, user._id);
       });
-
       passport.deserializeUser((id, done) => {
-      db.collection("users").findOne(
-      { _id: new ObjectID(id) },
-      (err, user) => {
-        done(null, user);
-      }
-    );
+        db.collection("users").findOne(
+          { _id: new ObjectID(id) },
+          (err, user) => {
+            done(null, user);
+          }
+        );
       });
 
       passport.use(
         new LocalStrategy(function(username, password, done) {
-          db.collection("users").findOne({ username: username }, function(err,user) {
+          db.collection("users").findOne({ username: username }, function(
+            err,
+            user
+          ) {
             console.log("User " + username + " attempted to log in.");
             if (err) {
               return done(err);
@@ -110,29 +110,50 @@ mongo.connect(
           });
         })
       );
-      
+      var login_fail = false;
       function ensureAuthenticated(req, res, next) {
         if (req.isAuthenticated()) {
           return next();
         }
         console.log("user not authenticated");
-        res.redirect("/");
+        req.session.login_fail = true;
+        res.redirect("/"); //, {
+        //   message:"login failed, please try again",
+        //   showLogin: true,
+        //   showRegistration:true
+        // });
       }
 
       app.route("/").get((req, res) => {
         //Change the response to render the Pug template
-        res.render(process.cwd() + "/views/pug/index", {
-          title: "Home Page",
-          message: "login",
-          showLogin: true,
-          showRegistration: true
-        });
+        if (req.session.login_fail) {
+          res.render(process.cwd() + "/views/pug/index", {
+            title: "Home Page",
+            message: "login Failed, please try again",
+            showLogin: true,
+            showRegistration: true
+          });
+        } else {
+          res.render(process.cwd() + "/views/pug/index", {
+            title: "Home Page",
+            message: "login",
+            login_fail: login_fail,
+            message_fail: "login failed, please try again",
+            showLogin: true,
+            showRegistration: true
+          });
+        }
       });
 
       app
         .route("/login")
         .post(
-          passport.authenticate("local", { failureRedirect: "/" }),
+          passport.authenticate("local", {
+            failureRedirect: "/",
+            message_fail: "login failed, please try again",
+            showLogin: true,
+            showRegistration: true
+          }),
           (req, res) => {
             console.log("post to /login with " + JSON.stringify(req.body));
             // might need to store user on 'session' variable? var req.session.user=req.user;
@@ -145,14 +166,16 @@ mongo.connect(
       app.route("/profile").get(ensureAuthenticated, (req, res) => {
         const username = req.session.username;
         delete req.session.username;
-        res.render('/app/views/pug/profile', {username:req.user.username, title:"Profile"});
+        res.render("/app/views/pug/profile", {
+          username: req.user.username,
+          title: "Profile"
+        });
       });
 
-      
-      app.route('/register')
-      .post((req, res, next) => {
+      app.route("/register").post(
+        (req, res, next) => {
           //    var hash = bcrypt.hashSync(req.body.password, 12);
-          db.collection('users').findOne(
+          db.collection("users").findOne(
             { username: req.body.username },
             (err, user) => {
               if (err) {
@@ -162,7 +185,7 @@ mongo.connect(
                   JSON.stringify(user.username) + " Already exists in Db"
                 );
                 req.session.username = user.username;
-                res.redirect("/");  // I think should be redirected to /profile...but pass tests
+                res.redirect("/"); // I think should be redirected to /profile...but pass tests
                 // res.render(process.cwd() + "/views/pug/index", {
                 //       title: 'Hello',
                 //       message: 'Already a member',
@@ -176,9 +199,9 @@ mongo.connect(
                   },
                   (err, user) => {
                     if (err) {
-                      res.redirect("/", );
+                      res.redirect("/");
                     } else {
-                       req.session.username = user.username;
+                      req.session.username = user.username;
                       next(null, user);
                     }
                   }
@@ -189,7 +212,6 @@ mongo.connect(
         },
         passport.authenticate("local", { failureRedirect: "/" }),
         (req, res, next) => {
-          
           res.redirect("/profile");
         }
       );
@@ -199,16 +221,21 @@ mongo.connect(
         req.logout();
         res.redirect("/");
       });
-      
-      app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
-        });
 
-      app.listen(process.env.PORT || 3000, () => {
-        console.log("Listening on port " + process.env.PORT);
+      app.use((req, res, next) => {
+        res
+          .status(404)
+          .type("text")
+          .send("Not Found");
       });
     }
   }
-);
+); //.catch((e) => {
+//   app.route('/').get((req, res) => {
+//     res.render('pug', { title: e, message: 'Unable to login' });
+//   });
+// });
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + process.env.PORT);
+});
